8dbc4a50e14d7bad99bd8673f0f3f493967464ae
Roger Ho <pingche2@illinois.edu>
2018-12-08 00:40:25 -0600

    Created basic database model foundation

--- ho_roger_final_project/BattleWeb101/admin.py
+++ ho_roger_final_project/BattleWeb101/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Player, Grid
+
+admin.site.register(Player)
+admin.site.register(Grid)
--- ho_roger_final_project/BattleWeb101/models.py
+++ ho_roger_final_project/BattleWeb101/models.py
@@ -4,22 +4,24 @@ from django.urls import reverse
 
 class Player(models.Model):
     player_id = models.AutoField(primary_key=True)
-    player_bullets = models.IntegerField()
-    player_hit_points = models.IntegerField()
-    player_suncker_points = models.IntegerField()
-    pass
+    # Limit players account name to length of 15 (would be enforced during signup)
+    player_name = models.CharField(max_length=15)
+    player_bullets = models.IntegerField(default=3)
+    player_hit_points = models.IntegerField(default=20)
+    player_suncker_points = models.IntegerField(default=0)
+
+    def __str__(self):
+        return "Name: %s. ID: %s" % (self.player_name, self.player_id)
+
+    class Meta:
+        ordering = ['player_id']
 
 
 class Grid(models.Model):
+    player = models.ForeignKey(Player, related_name='grid', on_delete=models.PROTECT)
     grid_x = models.IntegerField()
     grid_y = models.IntegerField()
 
-    pass
+    class Meta:
+        ordering = ['grid_x', 'grid_y']
 
-
-class HallOfFame(models.Model):
-    pass
-
-
-class HallOfSunckers(models.Model):
-    pass
